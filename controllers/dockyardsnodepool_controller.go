d082641c15b670054ee70bf816de86f533dfc743
Adam Hellstrom <adam.hellstrom@sudosweden.com>
2026-04-15 12:33:04 +0200

    Add back the rbac for nodepool

--- controllers/dockyardsnodepool_controller.go
+++ controllers/dockyardsnodepool_controller.go
@@ -56,7 +56,7 @@ import (
 // +kubebuilder:rbac:groups=cluster.x-k8s.io,resources=machinedeployments,verbs=create;get;list;patch;watch
 // +kubebuilder:rbac:groups=controlplane.cluster.x-k8s.io,resources=taloscontrolplanes,verbs=create;get;list;patch;watch
 // +kubebuilder:rbac:groups=dockyards.io,resources=clusters,verbs=get;list;watch
-// +kubebuilder:rbac:groups=dockyards.io,resources=nodepools,verbs=get;list;watch
+// +kubebuilder:rbac:groups=dockyards.io,resources=nodepools,verbs=get;list;watch;patch;update
 // +kubebuilder:rbac:groups=dockyards.io,resources=nodepools/status,verbs=patch;update
 // +kubebuilder:rbac:groups=dockyards.io,resources=releases,verbs=get;list;watch
 // +kubebuilder:rbac:groups=gateway.networking.k8s.io,resources=tlsroutes,verbs=get;list;watch
